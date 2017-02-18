00e8adb5ff8289ec7e898d75872bd6a0bb6314be
łukasz jabłoński <lukasz.j.pl@interia.pl>
2017-02-18 19:43:01 +0100

    zadanie_3

--- selenium-training/java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ selenium-training/java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -10,33 +10,35 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
-public class MyFirstTest  {
+public class MyFirstTest {
 
     private WebDriver driver;
     private WebDriverWait wait;
 
 
-    @Before
-    public void start(){
-        driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, 10);
 
-    }
+        @Before
+        public void start() {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, 10);
 
-    @Test
-    public void myFirstTest() {
-        driver.navigate().to("http://www.google.com");
-        driver.findElement(By.name("q")).sendKeys("webdriver");
-        driver.findElement(By.name("btnG")).click();
-        wait.until(titleIs("webdriver - Szukaj w Google"));
-                                //should i use english version or it is no problem in polish for trener ?
+        }
 
-    }
-     @After
-     public void stop(){
-         driver.quit();
-         driver = null;
+        @Test
+        public void myFirstTest() {
+            driver.navigate().to("http://localhost/litecart/admin");
+            driver.findElement(By.xpath("//*[@id=\"box-login\"]/form/div[1]/table/tbody/tr[1]/td[2]/span/input")).sendKeys("admin");
+            driver.findElement(By.xpath("//*[@id=\"box-login\"]/form/div[1]/table/tbody/tr[2]/td[2]/span/input")).sendKeys("admin");
+            driver.findElement(By.xpath("//*[@id=\"box-login\"]/form/div[2]/button")).click();
+
+
+        }
+        @After
+        public void stop() {
+            driver.quit();
+            driver = null;
         }
 
 
-}
+    }
+	  
