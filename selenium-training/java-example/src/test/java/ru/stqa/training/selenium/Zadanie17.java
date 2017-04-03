47226f03a8b402bf04318ab85a1bf21d5498815e
łukasz jabłoński <lukasz.j.pl@interia.pl>
2017-04-03 20:35:55 +0200

    Zadanie17
    Zadanie 17. Sprawdź w logach przeglądarki nieobecność komunikatów
    Zrób scenariusz, który sprawdza, czy w logach przeglądarki przy
    otwieraniu stron w aplikacji szkoleniowej nie pojawiają się jakieś
    komunikaty, chodzi konkretnie o strony towarów w katalogu w panelu
    administracyjnym.
    Skrypt powinien składać się z następujących części:
    1) wejść w panel administracyjny,
    2) otwarcie w katalogu kategorii, która zawiera towary
    
    (http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1),
    3) konsekwentne otwieranie stron towarów i sprawdzanie, czy nie
    pojawiają się w logach przeglądarki komunikaty (dowolnego poziomu),
    Możesz przygotować scenariusz jako test, albo jako oddzielny plik
    wykonywalny.

---
+++ selenium-training/java-example/src/test/java/ru/stqa/training/selenium/Zadanie17.java
@@ -0,0 +1,77 @@
+package ru.stqa.training.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+//import org.openqa.selenium.support.events.EventFiringWebDriver;
+//import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by luk on 2017-04-03.
+ */
+public class Zadanie17 {
+    /*
+    public static class MyListener extends AbstractWebDriverEventListener {
+        @Override
+        public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+            System.out.println(by);
+        }
+
+        @Override
+        public void afterFindBy(By by, WebElement element, WebDriver driver) {
+            System.out.println(by + " found");
+        }
+
+        @Override
+        public void onException(Throwable throwable, WebDriver driver) {
+            System.out.println(throwable);
+        }
+    }
+    */
+
+    private WebDriver driver;
+
+
+    @Before
+    public void before() {
+        driver = new ChromeDriver();
+        driver.manage().logs().get("browser").forEach(l -> System.out.println(l));
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void test() {
+        driver.navigate().to("http://localhost/litecart/admin/");
+        driver.findElement(By.cssSelector("div.content input[name=username]")).sendKeys("admin");
+        driver.findElement(By.cssSelector("div.content input[name=password]")).sendKeys("admin");
+        driver.findElement(By.cssSelector("#box-login-wrapper button[name=login]")).click();
+
+        driver.findElement(By.xpath("//ul[@id='box-apps-menu']//span[contains(text(),'Catalog')]")).click();
+        driver.findElement(By.xpath("//table[@class='dataTable']//a[contains(text(),'Rubber Ducks')]")).click();
+
+        List<WebElement> listOfProduct = driver.findElements(By.xpath("//table[@class='dataTable']//a[contains(text(),'Duck')]"));
+        String[] hrefOfProduct = new String[listOfProduct.size()];
+        for (int i = 0; i < listOfProduct.size(); i++) {
+            hrefOfProduct[i] = listOfProduct.get(i).getAttribute("href");
+
+        }
+        for (int j = 0; j < hrefOfProduct.length; j++) {
+            driver.navigate().to(hrefOfProduct[j]);
+        }
+
+    }
+    @After
+    public void after(){
+        driver.quit();
+        driver=null;
+    }
+
+}
